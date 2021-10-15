5c84e8323517c78a8feb8dd764d8bbf27af1865e
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-10-15 12:48:27 -0300

    profundizando useEffect

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -8,7 +8,7 @@ const CounterWithCustomHook = () => {
 
     return (
         <>
-            <h1>Counter with Hoolk: { state }</h1>
+            <h1>Counter with Hook: { state }</h1>
             <hr />
             <button className="btn btn-danger" onClick={ () => decrement(2) }> - 1 </button>
             <button className="btn btn-secondary" onClick={ reset }> reset </button>
---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import './useEffect.css';
+
+const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+
+    const { name, email } = formState;
+
+    // con useEffect puedo escuchar cambios especificos, segun que le pase en la dependencia.
+
+    useEffect ( () => {
+
+        console.log('hey!!')
+    }, [])
+    
+    useEffect ( () => {
+
+        console.log('formState Cambio')
+    }, [formState])
+    
+    useEffect ( () => {
+
+        console.log('email Cambio')
+    }, [email])
+
+    const handleOnChange = ({target}) =>{
+        
+        setFormState({
+            ...formState,
+            [ target.name ]: target.value 
+        })
+    }
+    
+    return (
+        <>
+            <h1>SimpleForm UseEffect</h1>
+            <hr/>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control mb-3"
+                    placeholder="Tu Nombre"
+                    autoComplete="off"
+                    value= {name}
+                    onChange= { handleOnChange }
+                />
+
+            </div>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="tuemail@email.com"
+                    autoComplete="off"
+                    value= { email }
+                    onChange= { handleOnChange }
+                />
+
+            </div>
+        </>
+    )
+}
+
+export default SimpleForm
