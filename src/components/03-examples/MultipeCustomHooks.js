67be28d3a092e8195aa4c8c402ef34680f911d51
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-10-19 00:41:41 -0300

    uso de multiples custom hooks

--- src/components/03-examples/MultipeCustomHooks.js
+++ src/components/03-examples/MultipeCustomHooks.js
@@ -1,21 +1,50 @@
 import React from 'react'
+import { useCounter } from '../../hooks/useCounter'
 import { useFetch } from '../../hooks/useFetch'
+
+
 import './multipleCustomHooks.css'
 
 const MultipeCustomHooks = () => {
 
-    const state = useFetch( 'https://www.breakingbadapi.com/api/quotes/1' );
-    console.log(state);
-    const { data } = state
-    console.log(data[0].quote)
-    const { quote, author } = data[0];
+    const { counter, increment } = useCounter(1)
+
+    const { loading, data } = useFetch( `https://www.breakingbadapi.com/api/quotes/${ counter }` );
+    console.log(data);
+
+    const { author, quote } = !!data && data[0];
+   
+   
 
     return (
-        <div>
-            <h1> { author }</h1>
-            <p> { quote } </p>
+        <>
+            <h1>Braiking Bad Quotes</h1>
+            { loading ? <div className="alert alert-info text-center">
+                             Loading...
+                        </div> 
+                      :
+                      (
+                        <div>
+                            <blockquote className="blockquote text-end">
+                                <p className="mb-3">
+                                    { quote }
+                                </p>
+                                <footer className="blockquote-footer">
+                                    { author }
+                                </footer>
+
+
+                            </blockquote>
+                            <button className="btn btn-primary text-start" onClick={ increment }>
+                                siguiente quote
+                            </button>
+                        </div>
+                        )
+            }
             
-        </div>
+            
+            
+        </>
     )
 }
 
