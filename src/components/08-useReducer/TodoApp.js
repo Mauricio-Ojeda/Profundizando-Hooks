347c905f32c2709e9d0090e99532378b55576538
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-10-26 23:49:40 -0300

    uso de dispatch( action ) add

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -12,9 +12,26 @@ const initialState = [{
 const TodoApp = () => {
 
 
-    const [ todos ] = useReducer(todoReducer, initialState)
+    const [ todos, dispatch ] = useReducer(todoReducer, initialState)
 
-    console.log(todos);
+    const handleOnSubmit = ( e ) => {
+        console.log(todos);
+        e.preventDefault();
+        console.log('handle on sumbit')
+        const newTodo = {
+                id: new Date().getTime(),
+                desc: 'Aprender BackEnd',
+                done: false
+        };
+
+        const action = {
+            type: 'add',
+            payload: newTodo
+        };
+        
+        dispatch( action )
+    }
+
     return (
         <div className="container mt-4">
             <h1>TodoApp ( { todos.length } )</h1>
@@ -24,19 +41,19 @@ const TodoApp = () => {
                         <ul className="list-group list-group-flush">
                             {
                                 todos.map( ({ id, desc }, index) => (
-                                <li key={id} className="list-group-item" >
-                                        <div className="row row-li">
-                                            <div className="col-6">
-                                                <p className="text-center"> {index + 1}. { desc }  </p>
+                                    <li key={id} className="list-group-item " >
+                                            <div className="row row-li p-3">
+                                                <div className="col-8">
+                                                    <p className="text-left"> {index + 1}. { desc }  </p>
 
-                                            </div>
-                                            <div className="col-6">
-                                                <button className="btn btn-danger">Borrar</button>
+                                                </div>
+                                                <div className="col-4 ">
+                                                    <button className="btn btn-outline-danger d-flex justify-content-end">Borrar</button>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                        
-                                </li>
+                                            
+                                    </li>
                                 ))
                             }
                         </ul>
@@ -44,9 +61,9 @@ const TodoApp = () => {
                 <div className="col-md-4">
                             <h4>Agregar Todo</h4>
                             <hr />
-                            <form className="form-group">
+                            <form className="form-group" onSubmit={ handleOnSubmit }>
                                 <input type="text" className="form-control mb-3" />
-                                <button className="btn btn-block btn-success">Agregar</button>
+                                <button type="submit" className="btn btn-block btn-outline-success">Agregar</button>
                             </form>
                 </div>
             </div>
--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -2,9 +2,9 @@
 export const todoReducer = ( state = [], action ) => {
     
     switch (action.type) {
-        // case 'add':
+        case 'add':
             
-        //     break;
+            return [ ...state, action.payload ];
     
         default:
             return state
